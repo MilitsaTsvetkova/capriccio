7b9fd72681f83981e5c58b90aa741cf3165252b1
MilitsaTsvetkova <militsa88@yahoo.com>
2025-03-01 14:59:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h1>Notion clone</h1>;
+}
+
+export default App;
---
+++ src/Page/Cover.tsx
@@ -0,0 +1,5 @@
+import styles from "./Cover.module.css";
+
+export const Cover = () => {
+  return <div className={styles.cover}></div>;
+};
