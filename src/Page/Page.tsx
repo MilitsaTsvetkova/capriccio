d515996acee85c1e66635f0310798d4581a808fc
MilitsaTsvetkova <militsa88@yahoo.com>
2025-03-03 11:47:32 +0200

    basic node

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,3 +1,5 @@
+import { BasicNode } from "../Node/BasicNode";
+import { NodeData } from "../utils/types";
 import { Cover } from "./Cover";
 import { Title } from "./Title";
 
@@ -6,6 +8,27 @@ export const Page = () => {
     <>
       <Cover />
       <Title title="Node" onChangeTitle={() => {}} addNode={() => {}} />
+      <BasicNode
+        node={{
+          id: "",
+          type: "text",
+          value: "",
+        }}
+        updateFocusedIndex={function (index: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        isFocused={false}
+        index={0}
+        addNode={function (node: NodeData, index: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        removeNodeByIndex={function (index: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        changeNodeValue={function (index: number, value: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
     </>
   );
 };
