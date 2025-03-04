3bab58ebc0527c28622d5f30064ddba41eddc1fc
MilitsaTsvetkova <militsa88@yahoo.com>
2025-03-04 09:09:09 +0200

    use AppStateContext in Page component

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,42 +1,22 @@
 import { nanoid } from "nanoid";
-import { useState } from "react";
 import { BasicNode } from "../Node/BasicNode";
-import { NodeData } from "../utils/types";
+import { useAppState } from "../state/AppStateContext";
 import { Cover } from "./Cover";
 import { Spacer } from "./Spacer";
 import { Title } from "./Title";
 import { useFocusedNodeIndex } from "./useFocusedNodeIndex";
 
 export const Page = () => {
-  const [nodes, setNodes] = useState<NodeData[]>([]);
+  const { nodes, title, setTitle, addNode } = useAppState();
   const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({
     nodes,
   });
-  const [title, setTitle] = useState("Default Title");
-
-  const addNote = (node: NodeData, index: number) => {
-    const newNodes = [...nodes];
-    newNodes.splice(index, 0, node);
-    setNodes(newNodes);
-  };
-
-  const removeNodeByIndex = (index: number) => {
-    const newNodes = [...nodes];
-    newNodes.splice(index, 1);
-    setNodes(newNodes);
-  };
-
-  const changeNodeValue = (index: number, value: string) => {
-    const newNodes = [...nodes];
-    newNodes[index].value = value;
-    setNodes(newNodes);
-  };
 
   return (
     <>
       <Cover />
       <div>
-        <Title title={title} onChangeTitle={setTitle} addNode={addNote} />
+        <Title title={title} onChangeTitle={setTitle} addNode={addNode} />
         {nodes.map((node, index) => (
           <BasicNode
             key={node.id}
@@ -44,15 +24,12 @@ export const Page = () => {
             updateFocusedIndex={setFocusedNodeIndex}
             isFocused={focusedNodeIndex === index}
             index={index}
-            addNode={addNote}
-            removeNodeByIndex={removeNodeByIndex}
-            changeNodeValue={changeNodeValue}
           />
         ))}
         <Spacer
           showHint={!nodes.length}
           onClick={() => {
-            addNote(
+            addNode(
               {
                 id: nanoid(),
                 type: "text",
