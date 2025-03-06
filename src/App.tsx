f959908dd71beb6eef9fc68322c5811777496cf6
MilitsaTsvetkova <militsa88@yahoo.com>
2025-03-06 10:02:31 +0200

    added Private component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Auth } from "./auth/Auth";
+import { Private } from "./auth/Private";
 import { Page } from "./Page/Page";
 import { AppStateProvider } from "./state/AppStateContext";
 import { createPage } from "./utils/createPage";
@@ -12,17 +13,25 @@ function App() {
       <Route
         path="/:id"
         element={
-          <AppStateProvider initialState={createPage()}>
-            <Page />
-          </AppStateProvider>
+          <Private
+            component={
+              <AppStateProvider initialState={createPage()}>
+                <Page />
+              </AppStateProvider>
+            }
+          />
         }
       />
       <Route
         path="/"
         element={
-          <AppStateProvider initialState={createPage()}>
-            <Page />
-          </AppStateProvider>
+          <Private
+            component={
+              <AppStateProvider initialState={createPage()}>
+                <Page />
+              </AppStateProvider>
+            }
+          />
         }
       />
     </Routes>
