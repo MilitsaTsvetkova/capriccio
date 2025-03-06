1c9afb32eb64858ae53434750f159b19026d3404
MilitsaTsvetkova <militsa88@yahoo.com>
2025-03-06 09:03:14 +0200

    added routes and react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Page } from "./Page/Page";
 import { AppStateProvider } from "./state/AppStateContext";
 import { createPage } from "./utils/createPage";
 
+const Auth = () => {
+  return <></>;
+};
+
 function App() {
   return (
-    <AppStateProvider initialState={createPage()}>
-      <Page />
-    </AppStateProvider>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+      <Route
+        path="/:id"
+        element={
+          <AppStateProvider initialState={createPage()}>
+            <Page />
+          </AppStateProvider>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <AppStateProvider initialState={createPage()}>
+            <Page />
+          </AppStateProvider>
+        }
+      />
+    </Routes>
   );
 }
 
